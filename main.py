de5b74aa218468c70738477a14b7631f579a77e8
Kaleo Sato <kaleosato@Kaleos-MacBook-Pro.local>
2018-01-20 00:05:30 -0800

    wrote barebones of program to parse csv file that yash will write

---
+++ main.py
@@ -0,0 +1,16 @@
+player_stats = ()
+team_stats = {}
+
+def read_csv():
+    csv_file = open('yash.csv', 'r')
+
+    #update the values of the dictionaries for each player and team
+    
+    csv_file.close()
+
+def calculate_points_to_score(team):
+    #calculates the points that the player will score against a team.
+    pass
+
+if __name__ == '__main__':
+    read_csv()
