04234b62e05f00721d48c823968b481c117e8565
cvraut <craut@uci.edu>
2018-01-20 00:48:10 -0800

    added missing json's
    added 1's & -1's

--- data/getdata.py
+++ data/getdata.py
@@ -1,10 +1,10 @@
 import urllib
 master_url = "https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2017/scores/gamedetail/0021700{}_gamedetail.json"
-i = 0.001
+i = 0.0011
 while i < 0.668:
-    f = urllib.urlopen(master_url.format(str(i)[2:]))
-    infile = open("game{}.json".format(str(i)[2:]),"w")
+    f = urllib.urlopen(master_url.format(str(i)[2:-1]))
+    infile = open("game{}.json".format(str(i)[2:-1]),"w")
     infile.write(f.read())
     infile.close()
-    print i
+    print i, "game{}.json".format(str(i)[2:-1]),"w"
     i+=0.001
