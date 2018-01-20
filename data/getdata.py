508eb308f19f7362c432185ec82008dae47ef81c
Yash Rane <yash_rane@umail.ucsb.edu>
2018-01-20 00:11:48 -0800

    Initial Data scraped from nba.com

---
+++ data/getdata.py
@@ -0,0 +1,10 @@
+import urllib
+master_url = "https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2017/scores/gamedetail/0021700{}_gamedetail.json"
+i = 0.001
+while i < 0.668:
+    f = urllib.urlopen(master_url.format(str(i)[2:]))
+    infile = open("game{}.json".format(str(i)[2:]),"w")
+    infile.write(f.read())
+    infile.close()
+    print i
+    i+=0.001
